afb3f2423d6a7d8b6054599bd3478392a6c20752
Забелин Михаил Константинович <zabelin.mihail@wildberries.ru>
2020-11-14 09:31:57 +0300

    сделано задание 1

---
+++ Lesson01.java
@@ -0,0 +1,12 @@
+package lesson01;
+
+import lesson012.*;
+
+public class Lesson01 {
+    public static void main(String[] args) {
+        Lesson012 lesson = new Lesson012();
+        for (var arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
---
+++ Lesson012.java
@@ -0,0 +1,7 @@
+package lesson012;
+
+public class Lesson012 {
+    public Lesson012() {
+        System.out.println("Hello, world");
+    }
+}
