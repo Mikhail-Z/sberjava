bf8cf90387e61e88eb3fbacbfab0dc02cb5deacb
Mikhail Zabelin <mike.rzn@yandex.ru>
2020-12-08 11:22:13 +0300

    убран sleep

--- multithreading/src/main/java/com/company/RequestHandler.java
+++ multithreading/src/main/java/com/company/RequestHandler.java
@@ -23,12 +23,6 @@ public class RequestHandler extends Thread {
                 break;
             }
             handleRequest(newRequest);
-            try {
-                sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                break;
-            }
         }
     }
 
