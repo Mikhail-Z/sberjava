9362826f38df4b0b539e6ae64fe9a6b46f433d56
Mikhail Zabelin <mike.rzn@yandex.ru>
2020-12-25 23:28:24 +0300

    сделано задание с расчетом средней стоимости акций

--- scala-example-for-school-one/src/main/scala/collection/CollectionIssueThree.scala
+++ scala-example-for-school-one/src/main/scala/collection/CollectionIssueThree.scala
@@ -1,35 +1,67 @@
 package collection
-
-import java.util.Date
+import java.time.LocalDate
+import java.time.format.{DateTimeFormatter, DateTimeFormatterBuilder}
 
 /**
  * Считать из файла hdata_stock.csv данные в буфер
  * Преобразовать считанные данные в Tuple5
  * Вывести построчно в консоль считанные полученный результат
  */
-
-class StockData( val date: Date,
-                 val googleStockPrice: BigDecimal,
-                 val microsoftStockPrice: BigDecimal,
-                 val amazonStockPrice: BigDecimal,
-                 val facebookStockPrice: BigDecimal) {
-}
-
 object CollectionIssueThree {
 
   def main(args: Array[String]): Unit = {
-    printlnStocksInfo("hdata_stock.csv")
+    val rawStockInfoLinesWithHeaders = readStocksInfo("hdata_stock.csv")
+    printStocksInfo(rawStockInfoLinesWithHeaders)
+    val stockInfoLines = rawStockInfoLinesWithHeaders.drop(1).map(x => parseStocksInfo(x))
+    val stockPrices = stockInfoLines.map(x => (x._2, x._3, x._4, x._5))
+    val avgStockPricesByCompany = getAvgStocksPrice(stockPrices)
+    println(avgStockPricesByCompany)
   }
 
-  def printlnStocksInfo(fileName: String): Unit = {
+  def readStocksInfo(fileName: String): Array[Tuple5[String, String, String, String, String]] = {
     val bufferedSource = io.Source.fromFile(fileName)
-    for (line <- bufferedSource.getLines()) {
-      val stock_prices = line.split(";").map(s => s.trim)
-      stock_prices match {
-        case Array(date, google, microsoft, amazon, facebook) => println(date, google, microsoft, amazon, facebook)
-        case _ =>
-      }
-    }
+    val stockInfoLines = bufferedSource.getLines()
+      .map(line => line.split(";").map(x => x.trim))
+      .filter(x => x.length == 5)
+      .map{
+        case Array(col1, col2, col3, col4, col5) => (col1, col2, col3, col4, col5)
+      }.toArray
+    //todo разобраться, почему этот вариант не работает
+    /*for (line <- bufferedSource.getLines()) {
+      val a = line.split(";")
+        .map(s => s.trim)
+        .collect{ case Array(col1, col2, col3, col4, col5) => (col1, col2, col3, col4, col5)}
+      }*/
     bufferedSource.close
+    stockInfoLines
+  }
+
+  def parseStocksInfo(stocksInfoPerDay: Tuple5[String, String, String, String, String]) = {
+    val dft = new DateTimeFormatterBuilder()
+      .appendOptional(DateTimeFormatter.ofPattern("dd.MM.yyyy kk:mm"))
+      .appendOptional(DateTimeFormatter.ofPattern("M/dd/yyyy kk:mm:ss"))
+      .toFormatter();
+    val dt = LocalDate.parse(stocksInfoPerDay._1, dft)
+    (dt,
+      BigDecimal(stocksInfoPerDay._2),
+      BigDecimal(stocksInfoPerDay._3),
+      BigDecimal(stocksInfoPerDay._4),
+      BigDecimal(stocksInfoPerDay._5))
+  }
+
+  def printStocksInfo(stocksInfoWithHeaders: Array[Tuple5[String, String, String, String, String]]): Unit = {
+    stocksInfoWithHeaders.foreach(x => println(x))
+  }
+
+  def getAvgStocksPrice(stockPrices: Array[Tuple4[BigDecimal, BigDecimal, BigDecimal, BigDecimal]]) = {
+    val initAcc = (BigDecimal(0), BigDecimal(0), BigDecimal(0), BigDecimal(0))
+    val sum = stockPrices.scanLeft(initAcc){
+      case ((acc1, acc2, acc3, acc4), (value1, value2, value3, value4)) => (
+        value1 + acc1,
+        value2 + acc2,
+        value3 + acc3,
+        value4 + acc4)}
+      .last
+    (sum._1/stockPrices.length, sum._2/BigDecimal(stockPrices.length), sum._3/stockPrices.length, stockPrices.length)
   }
 }
